7c4c536a882f21a2414990475ab6abb6c6ffe2a5
Ashley Donohoe <ashleyrdonohoe@gmail.com>
2016-09-15 20:58:38 -0400

    Added questionset

--- SpanishQuiz WatchKit Extension/Answer.swift
+++ SpanishQuiz WatchKit Extension/Answer.swift
@@ -13,7 +13,15 @@ class Model: NSObject {
     
     func getQuestionSet() -> [Answer] {
         let question1 = Answer(question: "Rojo", answers: ["red"])
+        let question2 = Answer(question: "Caliente", answers: ["hot", "warm"])
+        let question3 = Answer(question: "Perro", answers: ["dog"])
+        let question4 = Answer(question: "Cielo", answers: ["sky"])
+        let question5 = Answer(question: "Casa", answers: ["house", "home"])
         questionSets.append(question1)
+        questionSets.append(question2)
+        questionSets.append(question3)
+        questionSets.append(question4)
+        questionSets.append(question5)
         return questionSets
     }
 }
